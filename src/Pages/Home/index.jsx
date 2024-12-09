6be0a5e3440e376c8dd839c3833b440b34152640
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2024-12-08 21:31:21 -0500

    Mensaje describiendo los cambios realizados

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,50 +3,48 @@ import Layout from '../../Components/Layout'
 import '../../App.css'
 import Card from '../../Components/Card'
 import ProductDetail from '../../Components/ProductDetail'
-import {ShoppingCartContext} from '../../Context'
+import { ShoppingCartContext } from '../../Context'
 
 function Home() {
-  
-  const context=useContext(ShoppingCartContext)
+  const context = useContext(ShoppingCartContext)
 
-  const renderView=()=>{
-      if(context.filteredItems?.length>0){
-        return(
-          context.filteredItems?.map(item=>(
-            <Card key={item.id} data={item}/>
-          ))
-        )
-      }else{
-        return(
-          <div>We dont have anything</div>
-        )
-      }
-
+  const renderView = () => {
+    if (context.filteredItems?.length > 0) {
+      return context.filteredItems?.map(item => (
+        <Card key={item.id} data={item} />
+      ))
+    } else {
+      return <div>We dont have anything</div>
+    }
   }
- 
 
   return (
-      <Layout>
-        <div className='flex items-center justify-center relative w-80 '>
-          <h1 className='font-medium text-xl'>
-              Exclusive Products
-          </h1>
-        </div>
-        <input 
-          type="text" 
-          placeholder="Search a product" 
-          className="rounded-lg border border-black p-3 w-80 m-3 focus:border-[#FF6F61] focus:ring-2 focus:ring-[#FF6F61] focus:outline-none"
-          onChange={(event)=>{
-            console.log('Input value:', event.target.value);
-            context.setSearchByTitle(event.target.value)}}
-        />
+    <Layout>
+      {/* Título centrado */}
+      <div className="flex items-center justify-center relative w-full py-4">
+        <h1 className="font-medium text-xl">Exclusive Products</h1>
+      </div>
 
-        <div className='grid gap-5 grid-cols-4 w-full max-w-screen-lg'>
-          {renderView()}
-        </div>
-        <ProductDetail/>
-      </Layout>
+      {/* Input centrado en pantallas pequeñas */}
+      <input
+        type="text"
+        placeholder="Search a product"
+        className="rounded-lg border border-black p-3 w-80 mx-auto m-3 focus:border-[#FF6F61] focus:ring-2 focus:ring-[#FF6F61] focus:outline-none"
+        onChange={(event) => {
+          console.log('Input value:', event.target.value)
+          context.setSearchByTitle(event.target.value)
+        }}
+      />
 
+      {/* Contenedor de las cards con grid responsive centrado en pantallas pequeñas */}
+      <div className="flex flex-wrap justify-center gap-5 w-full max-w-screen-lg mx-auto">
+        {/* Usamos renderView para mostrar las cards */}
+        {renderView()}
+      </div>
+
+      {/* Detalle del producto */}
+      <ProductDetail />
+    </Layout>
   )
 }
 
